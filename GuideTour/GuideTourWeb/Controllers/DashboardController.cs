f2981a5b685d3291989ed312d49f49708c70aeb6
Florian Eder <florian.eder@pichlernetz.at>
2020-01-25 11:24:26 +0100

    Started with dashboard, removed home

---
+++ GuideTour/GuideTourWeb/Controllers/DashboardController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuideTourData.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuideTourWeb.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public readonly IDocumentDbRepository _ddb;
+       
+        public DashboardController(IDocumentDbRepository ddb)
+        {
+            _ddb = ddb;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Teams()
+        {
+            return View();
+        }
+
+        public IActionResult Guides()
+        {
+            return View();
+        }
+
+        public IActionResult Tours()
+        {
+            return View();
+        }
+    }
+}
