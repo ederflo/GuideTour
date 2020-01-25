dcd423d3389549d50b8a29596191afd9a70c88c4
Florian Eder <florian.eder@pichlernetz.at>
2020-01-25 12:13:24 +0100

    Removed old adminlte, Implemeted all dashboard stuff

--- GuideTour/GuideTourWeb/Helpers/ObjectHelpers/TourHelper.cs
+++ GuideTour/GuideTourWeb/Helpers/ObjectHelpers/TourHelper.cs
@@ -1,6 +1,7 @@
 ﻿using GuideTourData.Models;
 using GuideTourData.Services;
 using GuideTourLogic.Logics;
+using GuideTourWeb.Models;
 using GuideTourWeb.Models.MqttModels;
 using GuideTourWeb.Models.TourViewModels;
 using System;
